b394c4784affc41e705f7a04df03981c77c68cf3
kaviyarasan <kaviyarasan@way2smile.com>
2022-09-23 16:44:47 +0530

    form

--- src/Components/Add.js
+++ src/Components/Add.js
@@ -129,3 +129,195 @@ const onReset = () => {
 };
 
 export default AddForm;
+
+
+
+// import React, { useEffect, useState, useRef } from "react";
+// import "./AddForm.css";
+// import { useNavigate } from "react-router-dom";
+// import TextField from "@mui/material/TextField";
+
+
+// export default function AddForm() {
+ 
+
+//   return (
+//     <div>
+//               <div className="row">
+//                   <div className="col-6">
+//                       <div style={{ margin: "50px 36px" }}>
+//                             <div className="form-group studName_create_stud">
+//                               <div className="row">
+//                                 <div className="col-md-4">
+//                                   <label className="studLabel">
+//                                     Lead Name
+//                                   </label>
+//                                 </div>
+//                                 <div className="col-md-8">
+//                                   <TextField
+//                                     className="stud__name stud ml-3"
+//                                     name="username"
+//                                     variant="outlined"
+//                                     type="text"
+//                                   />
+//                                 </div>
+//                               </div>
+//                             </div>
+
+//                             <div className="form-group studName_create_stud student_id_wrapper student_id_label_center_wrapper">
+//                               <div className="row">
+//                                 <div className="col-md-4">
+//                                   <label className="studLabel">
+//                                     Lead Gender
+//                                   </label>
+//                                 </div>
+//                                 <div className="col-md-8">
+//                                   <TextField
+//                                     id="student_id"
+//                                     className="studId"
+//                                     type="text"
+//                                     name="student_unique_id"
+//                                   />
+//                                 </div>
+//                               </div>
+//                             </div>
+//                             <div className="form-group studName_create_stud">
+//                               <div className="row">
+//                                 <div className="col-md-4">
+//                                   <label className="studLabel">Lead Email</label>
+//                                 </div>
+//                                 <div className="col-md-8">
+//                                   <TextField
+//                                     id="email"
+//                                     className="studEmail"
+//                                     type="text"
+//                                     name="email"
+//                                   />
+//                                 </div>
+//                               </div>
+//                             </div>
+
+//                             <div className="form-group studName_create_stud">
+//                               <div className="row">
+//                                 <div className="col-md-4">
+//                                   <label className="studLabel">Lead Mobile</label>
+//                                 </div>
+//                                 <div className="col-md-8">
+//                                   <TextField
+//                                     id="mobile"
+//                                     className="studEmail"
+//                                     name="phone_number"
+//                                     type="number"
+//                                   />
+//                                 </div>
+//                               </div>
+//                             </div>
+
+//                             <div className="form-group studName_create_stud">
+//                               <div className="row">
+//                                 <div className="col-md-4">
+//                                   <label className="studLabel">Message</label>
+//                                 </div>
+//                                 <div className="col-md-8">
+//                                   <TextField
+//                                     id="mobile"
+//                                     className="studEmail"
+//                                     name="phone_number"
+//                                     type="text"
+//                                   />
+//                                 </div>
+//                               </div>
+//                             </div>
+//                       </div>
+//                   </div>
+
+//                   <div className="col-6">
+//                       <div style={{ margin: "50px 36px" }}>
+//                             <div className="form-group studName_create_stud">
+//                               <div className="row">
+//                                 <div className="col-md-4">
+//                                   <label className="studLabel">
+//                                     Employee Name
+//                                   </label>
+//                                 </div>
+//                                 <div className="col-md-8">
+//                                   <TextField
+//                                     className="stud__name stud ml-3"
+//                                     name="username"
+//                                     variant="outlined"
+//                                     type="text"
+//                                   />
+//                                 </div>
+//                               </div>
+//                             </div>
+
+//                             <div className="form-group studName_create_stud student_id_wrapper student_id_label_center_wrapper">
+//                               <div className="row">
+//                                 <div className="col-md-4">
+//                                   <label className="studLabel">
+//                                     Employee Gender
+//                                   </label>
+//                                 </div>
+//                                 <div className="col-md-8">
+//                                   <TextField
+//                                     id="student_id"
+//                                     className="studId"
+//                                     type="text"
+//                                     name="student_unique_id"
+//                                   />
+//                                 </div>
+//                               </div>
+//                             </div>
+//                             <div className="form-group studName_create_stud">
+//                               <div className="row">
+//                                 <div className="col-md-4">
+//                                   <label className="studLabel">Employee Email</label>
+//                                 </div>
+//                                 <div className="col-md-8">
+//                                   <TextField
+//                                     id="email"
+//                                     className="studEmail"
+//                                     type="text"
+//                                     name="email"
+//                                   />
+//                                 </div>
+//                               </div>
+//                             </div>
+
+//                             <div className="form-group studName_create_stud">
+//                               <div className="row">
+//                                 <div className="col-md-4">
+//                                   <label className="studLabel">Employee Mobile</label>
+//                                 </div>
+//                                 <div className="col-md-8">
+//                                   <TextField
+//                                     id="mobile"
+//                                     className="studEmail"
+//                                     name="phone_number"
+//                                     type="number"
+//                                   />
+//                                 </div>
+//                               </div>
+//                             </div>
+
+//                             <div className="form-group studName_create_stud">
+//                               <div className="row">
+//                                 <div className="col-md-4">
+//                                   <label className="studLabel">Start Rating</label>
+//                                 </div>
+//                                 <div className="col-md-8">
+//                                   <TextField
+//                                     id="mobile"
+//                                     className="studEmail"
+//                                     name="phone_number"
+//                                     type="number"
+//                                   />
+//                                 </div>
+//                               </div>
+//                             </div>
+//                       </div>
+//                   </div>
+//               </div>
+//     </div>
+//   );
+// }
